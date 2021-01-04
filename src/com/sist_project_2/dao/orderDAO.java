6b645c2c122f594ff003e48d52f1e3ed3c70145f
Dex_Park <mns0327@naver.com>
2021-01-04 11:19:23 +0900

    주문 조회 폼 리스트 완성

--- src/com/sist_project_2/dao/orderDAO.java
+++ src/com/sist_project_2/dao/orderDAO.java
@@ -1,8 +1,51 @@
 package com.sist_project_2.dao;
 
+import java.util.ArrayList;
+
 import com.sist_project_2.vo.orderVO;
 
 public class orderDAO extends DBConn{
+	
+	
+	
+	public ArrayList<orderVO> getOrderList() {
+		ArrayList<orderVO> list = new ArrayList<>();
+		
+		
+		try {
+			String sql = "select o.oid, m.name, o.rname, o.raddrnum, o.raddr, m.hp, o.rph, p.simg1, p.pname, p.pinfo, p.color, p.price, o.pcnt, o.total, to_char(o.rdate, 'yyyy/mm/dd') "
+					+ "from nibangmember m, nibangorder o, product p "
+					+ "where o.mid = m.mid and o.pid = p.pid";
+			getPreparedStatement(sql);
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				orderVO vo = new orderVO(); 
+				vo.setOid(rs.getString(1));
+				vo.setName(rs.getString(2));
+				vo.setRname(rs.getString(3));
+				vo.setRaddrnum(rs.getString(4));
+				vo.setRaddr(rs.getString(5));
+				vo.setHp(rs.getString(6));
+				vo.setRph(rs.getString(7));
+				vo.setSimg(rs.getString(8));
+				vo.setPname(rs.getString(9));
+				vo.setPinfo(rs.getString(10));
+				vo.setColor(rs.getString(11));
+				vo.setPrice(rs.getString(12));
+				vo.setPcnt(rs.getInt(13));
+				vo.setTotal(rs.getString(14));
+				vo.setRdate(rs.getString(15));
+				
+				list.add(vo);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return list;
+	}
 	
 	public orderVO getOrder() {
 		orderVO vo = new orderVO(); 
