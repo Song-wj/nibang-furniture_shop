7d032ce238adfd2b6d9ece98f43a52fc35042857
jy9oo <jhg2817@naver.com>
2021-01-06 15:47:13 +0900

    cartdao

--- src/com/sist_project_2/dao/cartDAO.java
+++ src/com/sist_project_2/dao/cartDAO.java
@@ -8,25 +8,25 @@ public class cartDAO extends DBConn{
       /**
        * insertCart : cart 등록
        */
-		
-		/*
-		 * public boolean insertCart(cartVO vo) { boolean result = false;
-		 * 
-		 * try {
-		 * 
-		 * String sql = "insert into cart values(?,?,?,sysdate)";
-		 * 
-		 * getPreparedStatement(sql); //pstmt.setString(1, vo.getMid());
-		 * pstmt.setString(1, "dd@gmail.com"); pstmt.setString(2, vo.getPid());
-		 * pstmt.setInt(3, vo.getC_qty());
-		 * 
-		 * int val = pstmt.executeUpdate(); if (val != 0) result = true;
-		 * 
-		 * } catch (Exception e) { e.printStackTrace(); }
-		 * 
-		 * return result; }
-		 */
-		
+      
+      /*
+       * public boolean insertCart(cartVO vo) { boolean result = false;
+       * 
+       * try {
+       * 
+       * String sql = "insert into cart values(?,?,?,sysdate)";
+       * 
+       * getPreparedStatement(sql); //pstmt.setString(1, vo.getMid());
+       * pstmt.setString(1, "dd@gmail.com"); pstmt.setString(2, vo.getPid());
+       * pstmt.setInt(3, vo.getC_qty());
+       * 
+       * int val = pstmt.executeUpdate(); if (val != 0) result = true;
+       * 
+       * } catch (Exception e) { e.printStackTrace(); }
+       * 
+       * return result; }
+       */
+      
       
       /**
        * getCart
@@ -67,77 +67,74 @@ public class cartDAO extends DBConn{
        * 삭제
        */
       public boolean cartDelete(String pid) {
-    	boolean result = false;
-  		try {
-  			String sql = "delete from cart where pid=?";
-  			getPreparedStatement(sql);
-  			pstmt.setString(1, pid);
-  			int val = pstmt.executeUpdate();
-	        if (val != 0) result = true;
-  			
-  		} catch (Exception e) {
-  			e.printStackTrace();
-  		}
-  		return result;
-  	}
+       boolean result = false;
+        try {
+           String sql = "delete from cart where pid=?";
+           getPreparedStatement(sql);
+           pstmt.setString(1, pid);
+           int val = pstmt.executeUpdate();
+           if (val != 0) result = true;
+           
+        } catch (Exception e) {
+           e.printStackTrace();
+        }
+        return result;
+     }
       
       /**
        * 선택 삭제
        */
       public boolean cartCheckedDelete(String[] pidList) {
-    	boolean result = false;
-    	String pidListStr = "";
-    	for(int i=0; i<pidList.length; i++) {
-    		pidListStr += "?";
-    		if(i != pidList.length-1) {
-    			pidListStr += ",";
-    		}		
-    	}
-  		try {
-  			String sql = "delete from cart where pid in ( " + pidListStr +" )";
-  			System.out.println(sql);
-  			getPreparedStatement(sql);
-  			for(int i=1; i<=pidList.length;i++) {
-  				pstmt.setString(i, pidList[i-1]);
-  			}
-  			int val = pstmt.executeUpdate();
-	        if (val != 0) result = true;
-  			
-  		} catch (Exception e) {
-  			e.printStackTrace();
-  		}
-  		return result;
-  	}
+       boolean result = false;
+       String pidListStr = "";
+       for(int i=0; i<pidList.length; i++) {
+          pidListStr += "?";
+          if(i != pidList.length-1) {
+             pidListStr += ",";
+          }      
+       }
+        try {
+           String sql = "delete from cart where pid in ( " + pidListStr +" )";
+           System.out.println(sql);
+           getPreparedStatement(sql);
+           for(int i=1; i<=pidList.length;i++) {
+              pstmt.setString(i, pidList[i-1]);
+           }
+           int val = pstmt.executeUpdate();
+           if (val != 0) result = true;
+           
+        } catch (Exception e) {
+           e.printStackTrace();
+        }
+        return result;
+     }
       
       /**
        * 업데이트
        */
 
-		
-		  public boolean insertCart(cartVO vo) { boolean result = false;
-		  
-		  try {
-		  
-		  String sql = "MERGE INTO cart USING DUAL ON (pid = ?) " +
-		  " WHEN MATCHED THEN " + " UPDATE SET " + " C_QTY = C_QTY + ? " +
-		  " WHEN NOT MATCHED THEN " +
-		  " INSERT (MID,PID,C_QTY,C_DATE) VALUES (?,?,?,sysdate)";
-		  
-		  getPreparedStatement(sql);
-		  pstmt.setString(1, vo.getPid());
-		  pstmt.setInt(2,vo.getC_qty());
-		  pstmt.setString(3, vo.getMid());
-		  pstmt.setString(4,vo.getPid());
-		  pstmt.setInt(5, vo.getC_qty());
-		  
-		  int val = pstmt.executeUpdate(); if (val != 0) result = true;
-		  
-		  } catch (Exception e) { e.printStackTrace(); }
-		  
-		  return result; }
-		 
+      
+        public boolean insertCart(cartVO vo) { boolean result = false;
+        
+        try {
+        
+        String sql = "MERGE INTO cart USING DUAL ON (pid = ?) " +
+        " WHEN MATCHED THEN " + " UPDATE SET " + " C_QTY = C_QTY + ? " +
+        " WHEN NOT MATCHED THEN " +
+        " INSERT (MID,PID,C_QTY,C_DATE) VALUES (?,?,?,sysdate)";
+        
+        getPreparedStatement(sql); pstmt.setString(1, vo.getPid()); pstmt.setInt(2,
+        vo.getC_qty()); pstmt.setString(3, vo.getMid()); pstmt.setString(4,
+        vo.getPid()); pstmt.setInt(5, vo.getC_qty());
+        
+        int val = pstmt.executeUpdate(); if (val != 0) result = true;
+        
+        } catch (Exception e) { e.printStackTrace(); }
+        
+        return result; }
+       
       
       
-	
+   
       
 }//cartDAO
