1379796b7c02e2eb86f960e3e15ee5702e940460
Song-wj <deepblack507@naver.com>
2021-01-07 09:15:11 +0900

    orderDAO수정

--- src/com/sist_project_2/dao/orderDAO.java
+++ src/com/sist_project_2/dao/orderDAO.java
@@ -305,11 +305,10 @@ public class orderDAO extends DBConn{
          pstmt.setString(9, vo.getRph());
          pstmt.setString(10, vo.getRrequest());
          pstmt.setString(11, vo.getReview_chk());
-<<<<<<< HEAD
-         pstmt.setString(11, vo.getOrder_chk());
-=======
-         pstmt.setString(12, "o");
->>>>>>> 4234bfc258e258222c4bcf0cb7182a524e8475f4
+         pstmt.setString(12, vo.getOrder_chk());
+
+ 
+
          
          int val = pstmt.executeUpdate();
          
